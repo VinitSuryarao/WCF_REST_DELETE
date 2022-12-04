1291cd67fd1520c3367e973d95069343ab7a0e89
Vinit Suryarao <suryarao.vinit@hotmail.com>
2022-12-04 10:29:27 +0530

    First Commit

---
+++ ClientMaster/Scripts/Service.js
@@ -0,0 +1,25 @@
+﻿function getAllClients() {
+    $.ajax({
+        url: "Service/ClientService.svc/GetAllClients",
+        type: "GET",
+        dataType: "json",
+        success: function (result) {
+            clients = result;
+            bindClientListToTable(clients);
+        }
+    });
+}
+
+
+function deleteClient() {
+    var id = $("#deleteId").val();
+    $.ajax({
+        url: "Service/ClientService.svc/DeleteClient/" + id,
+        type: "DELETE",
+        dataType: "json",
+        contentType: "application/json",
+        success: function (result) {
+            getAllClients();
+        }
+    });
+}
---
+++ ClientMaster/Service/ClientService.svc.cs
@@ -0,0 +1,33 @@
+﻿using ClientMaster.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace ClientMaster.Service
+{
+    [ServiceContract(Namespace = "")]
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class ClientService
+    {
+        [OperationContract, WebGet(ResponseFormat = WebMessageFormat.Json)]
+        public List<ClientModel> GetAllClients() // Get All Record
+        {
+            return ClientData.Data;
+        }
+
+        [OperationContract, WebInvoke(ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare,
+              UriTemplate = "DeleteClient/{Id}", Method = "DELETE")]
+        public ClientModel DeleteClient(string Id) // Delete Record
+        {
+            ClientModel obj = ClientData.Data.Where(x => x.Id == Convert.ToInt32(Id)).FirstOrDefault();
+            ClientData.Data.Remove(obj);
+
+            return obj;
+        }
+    }
+}
